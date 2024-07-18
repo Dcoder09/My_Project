a1ac7572a5f80e1086081acd36f898110cbd4dfe
Dcoder09 <148368666+Dcoder09@users.noreply.github.com>
2024-07-18 19:08:49 +0530

    improve calculaotr functionality

--- Calculator/script.js
+++ Calculator/script.js
@@ -1,4 +1,6 @@
 let string = "";
+let last_result="";
+let p = 0;
 let buttons = document.querySelectorAll("button");
 console.log(buttons);
 function fac(n) {
@@ -10,19 +12,46 @@ function fac(n) {
 }
 function calc(e) {
   let input_value = document.querySelector("input");
+
+  if (p == 1) {
+    string = "";
+    input_value.value = "";
+    p = 0;
+  }
   if (e.target.innerHTML == "=") {
     let arr = Array.from(string);
+    console.log(`${arr}`);
+
     let index = arr.indexOf("!");
     let num;
     if (index >= 0) {
+      if (isNaN(arr[arr.length - 2])) {
+        input_value.value = "Syntax Error";
+        p = 1;
+        return;
+      }
+
       arr.pop();
       num = parseFloat(arr.join(""));
       input_value.value = fac(num);
+      p = 1;
     } else {
-      string = eval(string.replace("^", "**"));
+      try {
+        string = eval(string.replace("^", "**"));
+      } catch (error) {
+        string = "Syntax Error";
+        p = 1;
+      }
       input_value.value = string;
+      last_result=string;
+      string = "";
     }
-  } else if (e.target.innerHTML == "AC") {
+  }
+  else if(e.target.innerHTML =="Res"){
+    string=string + last_result;
+    input_value.value = string;
+  }
+   else if (e.target.innerHTML == "AC") {
     string = "";
     input_value.value = string;
   } else {
